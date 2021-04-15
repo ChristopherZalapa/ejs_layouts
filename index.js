6b1a248c80afc0c7481c3da39869f2d7287d011b
ChristopherZalapa <chriszalapa@gmail.com>
2021-04-15 10:16:09 -0700

    First Init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+// Tell Express To Use EJS To Display Views
+app.set('view engine', 'ejs');
+
+// Home Page
+app.get('/', (req, res) => {
+	res.render('index.ejs', {
+		name: 'Sterling Archer',
+		age: 35,
+	});
+});
+
+// About Page
+app.get('/about', (req, res) => {
+	let data = ['LSD', 'House', 'Techno'];
+	res.render('about.ejs', {
+		favoriteThings: data,
+	});
+});
+
+// Blog Page
+app.get('/blog', (req, res) => {
+	res.render('blog-directory.ejs');
+});
+
+// Port Listen
+app.listen(3000, () => {
+	console.log('Now Listenting On Port 3000');
+});
